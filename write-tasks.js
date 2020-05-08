6fa940dd604e1efb2d2a00e22f4732edc99b4b3c
shivam7374 <shivamsingla491@gmail.com>
2020-05-08 19:38:34 +0530

    Add

--- write-tasks.js
+++ write-tasks.js
@@ -1,11 +1,15 @@
 const Sequelize=require('sequelize')
 
 const db = new Sequelize({
-    // mysql
-    dialect:'mysql',
-    database:'sampledb22',
-    username:'sampleuser22',
-    password:'samplepass22'
+     // mysql
+     dialect: 'mysql',
+     database: 'sampledb22',
+     username: 'sampleuser22',
+     password: 'samplepass22',
+     
+     // sqlite
+     // dialect: 'sqlite',
+     // storage: __dirname + '/tasks.db'
 })
 // Tasks is my model
 const Tasks = db.define('task', {
@@ -35,7 +39,12 @@ async function task(){
         {title: 'This is task 5'},
         {title: 'This is task 6'},
     ])
+    const tasks = await Tasks.findAll();
+    console.log('tasks found = ', tasks.length)
 
+    tasks.forEach((t) => {
+        console.log(`${t.id} \t ${t.done} \t ${t.title}`)
+    })
 
 }
 task()
